a054b59d1989e623661d832c451b6cba26486c3c
DanielHYung <69775739+DanielHYung@users.noreply.github.com>
2020-08-17 12:05:56 +1000

    60 percent complete

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+    public GameObject redObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadAssets redObj = gameObject.GetComponent<LoadAssets>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
